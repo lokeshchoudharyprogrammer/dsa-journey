47837a7b193662452ef3a63a0d4e553952311025
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-03 12:30:36 +0530

    solve some binary problem day-8 update and start day-9

--- day-8/book.cpp
+++ day-8/book.cpp
@@ -2,108 +2,164 @@
 #include <iostream>
 using namespace std;
 
-bool check(int arr[], int k, int limit, int n)
-{
-
-    int cnt = 1;
-    int pageSum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (pageSum + arr[i] > limit)
-        {
-            cnt++;
-            pageSum = arr[i];
-        }
-        else
-        {
-            pageSum += arr[i];
-        }
-    }
-    return (cnt <= k);
-}
-
 int findPages(int arr[], int k, int n)
 {
+    // code here
 
-    if (k > n)
-        return -1;
+    int m = k;
 
-    int lo = *max_element(arr.begin(), arr.end());
-    int hi = accumulate(arr.begin(), arr.end(), 0);
-    int res = -1;
-
-    while (lo <= hi)
+    if (m > n)
     {
-        int mid = lo + (hi - lo) / 2;
+        return -1;
+    }
 
-        if (check(arr, k, mid, n))
+    int start = 0, end = 0, mid, ans;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        start = max(start, arr[i]);
+        end += arr[i];
+    }
+
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+
+        int pages = 0, count = 1;
+
+        for (int k = 0; k < n; k++)
         {
-            res = mid;
-            hi = mid - 1;
+
+            pages += arr[k];
+
+            if (pages > mid)
+            {
+                count++;
+                pages = arr[k];
+            }
+        }
+
+        if (count <= m)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else
+            start = mid + 1;
+    }
+
+    return ans;
+}
+
+int printpartition(int arr[], int n, int k)
+{
+
+    int m = k;
+
+    int start = 0, end = 0, mid, ans;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (start < arr[i])
+        {
+            start = arr[i];
+        }
+        end += arr[i];
+    }
+
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+
+        int pages = 0, count = 1;
+
+        for (int k = 0; k < n; k++)
+        {
+
+            pages += arr[k];
+
+            if (pages > mid)
+            {
+                count++;
+                pages = arr[k];
+            }
+        }
+
+        if (count <= m)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else
+            start = mid + 1;
+    }
+
+    return ans;
+}
+
+int shipPackage(int arr[], int n, int k)
+{
+    int start = 0;
+    int end = 0;
+    for (int i = 0; i < n; i++)
+    {
+        start = max(start, arr[i]);
+        end += arr[i];
+    }
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        int pages = 0;
+        int count = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            pages += arr[i];
+            if (pages > mid)
+            {
+                count++;
+                pages = arr[i];
+            }
+        }
+        if (count <= k)
+        {
+            end = mid - 1;
         }
         else
         {
-            lo = mid + 1;
+            start = mid + 1;
         }
     }
-
-    return res;
-}
-
-int MAX_N = 100;
-int MAX_K = 100;
-
-int minTime(int arr[], int n, int k)
-{
-    // Declare dp array and initialize with -1
-    int dp[MAX_N][MAX_K];
-    memset(dp, -1, sizeof(dp));
-    return minimizeTime(0, n, arr, k, dp);
-}
-
-int minimizeTime(int curr, int n, int arr[], int k, int dp[MAX_N][MAX_K])
-{
-    // If all boards are painted, return 0
-    if (curr >= n)
-        return 0;
-
-    if (k == 0)
-        return INT_MAX;
-
-    if (dp[curr][k] != -1)
-        return dp[curr][k];
-
-    int currSum = 0;
-
-    int res = INT_MAX;
-
-    for (int i = curr; i < n; i++)
-    {
-        currSum += arr[i];
-
-        int remTime = minimizeTime(i + 1, n, arr, k - 1, dp);
-        int remaining = max(currSum, remTime);
-
-        res = min(res, remaining);
-    }
-
-    dp[curr][k] = res;
-
-    return res;
+    return start;
 }
 
 
 
 int main()
 {
+    // book allocation problem
     int arr[] = {12, 34, 67, 90};
     int k = 2;
     int n = sizeof(arr) / sizeof(arr[0]);
     cout << findPages(arr, k, n);
     cout << endl;
-    int  arrt[] = {10, 10, 10, 10};
-    int kt = 2;
-    int nt = sizeof(arr) / sizeof(arr[0]);
-    int res = minTime(arrt, nt, kt);
+    
+
+    // painter partition problem
+
+    int arrt[] = {5, 10, 30, 20, 15};
+    int kt = 3;
+    int nt = 5;
+    int res = printpartition(arrt, nt, kt);
     cout << res << endl;
+    // painter partition problem
+
+    int ship[] = {8,9,10};
+    int days = 3;
+    int shiplength = 3;
+    int shippkg = shipPackage(ship, shiplength, days);
+    cout << shippkg << endl;
 }
