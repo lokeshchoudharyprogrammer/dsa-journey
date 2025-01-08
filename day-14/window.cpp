0d66863b22bd63faa0298b631bb8b410f0dae033
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-08 23:10:51 +0530

    window code

---
+++ day-14/window.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int longestUniqueSubstr(string &s) {
+        // code here
+        vector<bool>count(256,0);
+    
+        int first=0,second=0,len=0;
+    
+        while(second<s.size()){
+            // repeating char
+    
+            while(count[s[second]]){
+                count[s[first]]=0;
+                first++;
+            }
+    
+            count[s[second]]=1;
+            len=max(len,second-first+1);
+            second++;
+        }
+        return len;
+}
+
+int main(){
+
+    // longestUniqueSubstr complete and define 
+
+    string s="abcdefabcbb";
+
+    
+    cout<<longestUniqueSubstr(s);
+
+
+    return 0;
+}
