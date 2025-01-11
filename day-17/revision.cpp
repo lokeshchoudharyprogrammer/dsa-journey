783d62a64a6d4c5407a806f43ee43a8dfd8f177b
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-11 21:49:28 +0530

    revision day ✌

---
+++ day-17/revision.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"Hello, World! Day-17" << endl;
+
+    cout << "this is revision day only revision and also solve some problems from geeksforgeeks and leet code    😊" << endl;
+
+}
