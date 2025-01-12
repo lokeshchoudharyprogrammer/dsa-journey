1d63d27d5e1e44de96ceb1696b653f36a9eeaa15
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-12 22:06:11 +0530

    revision day-2

---
+++ dsa-18/revisionday-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"Hello, World! Day-18" << endl;
+
+    cout << "this is revision day only revision and also solve some problems from geeksforgeeks and leet code    😊" << endl;
+
+}
