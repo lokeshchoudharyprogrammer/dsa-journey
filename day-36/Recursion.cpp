7e15afc3836633f1d89bb1ee39d4ed8e08634c39
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-30 23:46:51 +0530

    Recursion code

---
+++ day-36/Recursion.cpp
@@ -0,0 +1,38 @@
+// Recursion 
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <vector>
+
+std::vector<int> fibonacci(int n) {
+    if (n == 0) { 
+        return {};
+    } else if (n == 1) {
+        return {0};
+    } else if (n == 2) {
+        return {0, 1};
+    } else {
+        std::vector<int> fib = fibonacci(n-1);
+        fib.push_back(fib.back() + fib[fib.size()-2]);
+        return fib;
+    }
+}
+
+int factorial(int n) {
+    if (n == 0) { // base case
+        return 1;
+    } else {
+        return n * factorial(n-1); // recursive call
+    }
+}
+
+int main() {
+
+    // Recursion code 
+
+
+
+
+
+    return 0;
+}
