4066b6792d7e4a6f33d66efe92c2c9bf596ed9ac
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-03-01 23:49:31 +0530

    leet code que

---
+++ day-66/leetcode.js
@@ -0,0 +1,17 @@
+var plusOne = function (digits) {
+
+    let num = digits.join("")
+
+    let n = BigInt(num) + 1n
+
+    return n.toString().split("")
+
+};
+
+
+// 🗓️ Day-66 DSA Journey 🚀
+// ✔️ Took a deep dive into algorithmic concepts, reading articles to strengthen my problem-solving approach.
+// ✔️ Solved LeetCode - Plus One problem, focusing on edge cases and efficiency.
+// 🎯 Balancing revision, UI improvements, and problem-solving to sharpen my skills continuously!
+
+// #Day66  #DSA #LeetCode #Algorithms #UIImprovement #CodingJourney #AppOptimization 🚀
