c32935c83fb0e46bb94ee0c019e0eecdcb38342a
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-04-17 00:12:37 +0530

    linkedlist

---
+++ day-112/gfg.js
@@ -0,0 +1,57 @@
+// 🗓️ 𝐃𝐚𝐲 112 – 𝐋𝐢𝐧𝐤𝐞𝐝 𝐋𝐢𝐬𝐭 𝐆𝐫𝐢𝐧𝐝 + 𝐌𝐨𝐧𝐨𝐫𝐞𝐩𝐨 𝐔𝐩𝐠𝐫𝐚𝐝𝐞 🚀
+
+
+// https://www.geeksforgeeks.org/user/lokeshchoudharyprogrammer/ [16/04/2025] 
+
+
+// 📘 𝐃𝐒𝐀 𝐇𝐢𝐠𝐡𝐥𝐢𝐠𝐡𝐭𝐬: 𝐖𝐞𝐧𝐭 𝐟𝐮𝐥𝐥 𝐭𝐡𝐫𝐨𝐭𝐭𝐥𝐞 𝐨𝐧 𝐋𝐢𝐧𝐤𝐞𝐝 𝐋𝐢𝐬𝐭 𝐩𝐫𝐨𝐛𝐥𝐞𝐦𝐬 𝐭𝐨𝐝𝐚𝐲, 𝐤𝐧𝐨𝐜𝐤𝐞𝐝 𝐭𝐡𝐞𝐬𝐞 𝐨𝐮𝐭:
+
+
+
+// 🔗 Linked List Insertion At End
+
+//  🧮 Count Linked List Nodes
+
+//  🪞 Identical Linked Lists
+
+//  🖨️ Print Linked List
+
+//  📍 Insert in Middle of Linked List
+
+//  ⚖️ Is Linked List Length Even?
+
+//  🧩 Modular Node
+
+//  📋 Display Linked List
+
+//  🔍 Search In Linked List
+
+//  🔁 Doubly Linked List Traversal
+
+//  📉 Kth from End of Linked List
+
+//  🎯 Middle of a Linked List
+
+//  🔄 Reverse a Linked List
+
+//  ❌ Remove Duplicates from a Sorted List
+
+//  🔄 Find Length of Loop
+
+//  🧹 Remove Duplicates from an Unsorted List
+
+//  🗑️ Delete in a Singly Linked List
+
+//  🚫 Delete without Head Pointer
+
+//  🔗 Join Two Linked Lists
+
+//  ✂️ Delete Head of Linked List
+
+//  📥 Linked List Insertion At Beginning
+
+//  🔁 Remove every k'th Node
+
+//  📦 Array to Linked List
+
+//  ❌ Delete Middle of Linked List
