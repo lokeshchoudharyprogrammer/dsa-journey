8b62c78744c94f18b4b9f79b1aea3f12ff104bb6
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-04-24 00:09:26 +0530

    dsa

---
+++ day-119/leetcode.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/u/lokeshchoudharydev/ [254] 
+
+// 🗓️ 𝐃𝐚𝐲 119 – 𝐒𝐭𝐞𝐚𝐝𝐲 & 𝐒𝐡𝐚𝐫𝐩
+
+
+
+// 📘 𝐃𝐒𝐀 𝐏𝐫𝐨𝐠𝐫𝐞𝐬𝐬: Solved a solid set of problems across arrays, strings, and logic:
+
+// Unique Number of Occurrences
+
+// Minimize String Length
+
+// Number of Unequal Triplets in Array
+
+// Number of Arithmetic Triplets
+
+// Check Whether Two Strings are Almost Equivalent
+
+// Count Special Quadruplets
+
+// Count Number of Pairs With Absolute Difference K
+
+// Rotate String
+
+// Find All Numbers Disappeared in an Array
+
+// Goat Latin
+
+// Contains Duplicate II
+
