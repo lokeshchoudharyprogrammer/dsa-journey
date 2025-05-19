94185145e8cb91260f9b3fc12c36771c2a9ce20a
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-05-20 00:34:34 +0530

    dsa

---
+++ day-145/leetcode.js
@@ -0,0 +1,15 @@
+// 📌 𝐃𝐒𝐀
+
+// https://leetcode.com/u/lokeshchoudharydev/  [302 problems done]
+
+//  ✅ Solved 5 LeetCode problems:
+
+// Type of Triangle
+
+// Convert Sorted Array to Binary Search Tree
+
+// Intersection of Two Arrays II
+
+// Find Numbers with Even Number of Digits
+
+// Smallest Index With Equal Value
