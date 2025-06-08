023c2a7d08629a32e5f9524f12a866e24d3bacb3
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-06-09 00:53:48 +0530

    dsa

---
+++ day-164/dsa.js
@@ -0,0 +1,37 @@
+
+// 📅 𝐃𝐚𝐲 -𝟏𝟔𝟒: 𝐃𝐞𝐞𝐩 𝐃𝐢𝐯𝐞 𝐢𝐧𝐭𝐨 𝐁𝐢𝐧𝐚𝐫𝐲 𝐓𝐫𝐞𝐞𝐬 🌳🔁
+
+// Just checkout my coding journey on leetcode => https://leetcode.com/u/lokeshchoudharydev/
+//  and gfg => https://www.geeksforgeeks.org/user/lokeshchoudharyprogrammer/
+
+
+// 📘 DSA - Binary Tree Problems Solved:
+
+// ✅ Binary Tree Zigzag Level Order Traversal
+
+// ✅ Balanced Binary Tree
+
+// ✅ Invert Binary Tree
+
+// ✅ Count Complete Tree Nodes
+
+// ✅ Determine if Two Trees are Identical
+
+// ✅ Check for Balanced Tree
+
+// ✅ Size of Binary Tree
+
+// ✅ Sum of Binary Tree
+
+// ✅ Count Leaves in Binary Tree
+
+// ✅ Count Non - Leaf Nodes in Tree
+
+// ✅ Height of Binary Tree
+
+// ✅ Largest Value in Each Level
+
+
+
+// 🔁 𝐅𝐨𝐜𝐮𝐬: Strengthening tree - based recursion and traversal logic with hands - on problem solving.
+
